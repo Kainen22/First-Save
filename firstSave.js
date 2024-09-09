273d20852c4f378fe53ad3060447fa21f5034d69
Kainen Warren <kainenwarren@Kainens-MacBook-Air.local>
2024-09-09 15:39:33 -0700

    Punjab Virus

---
+++ firstSave.js
@@ -0,0 +1,13 @@
+//this wont break my code
+
+//variables
+
+//let vs const
+// let means it can be changed. const means it cant be changed
+
+// let camelCase = "Thing";
+
+let largePenis = "Kainen";
+
+// console.log(*thing you want to print*)
+console.log(largePenis);

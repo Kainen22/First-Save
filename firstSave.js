1db5621077a6a0aa29cc70ddb0c11ad31827f645
Kainen Warren <kainenwarren@Kainens-MacBook-Air.local>
2024-09-09 15:48:40 -0700

    fatty

--- firstSave.js
+++ firstSave.js
@@ -11,3 +11,20 @@ let largePenis = "Kainen";
 
 // console.log(*thing you want to print*)
 console.log(largePenis);
+console.log("test");
+
+
+//string "thing in quotes"
+//numbers 1232434
+//boolean true or false
+//arrays
+//objects
+//functions
+
+let age = 25;
+console.log(age)
+
+console.log(`I can type words and then randomly put a variable here: ${largePenis}`);
+
+console.log("this is largepenis", largePenis)
+

0b1d6137d5444ff0489d375ef12c4d0daf550637
Kainen Warren <kainenwarren@Kainens-MacBook-Air.local>
2024-09-09 16:20:19 -0700

    First one, wooo

--- firstSave.js
+++ firstSave.js
@@ -13,18 +13,53 @@ let largePenis = "Kainen";
 console.log(largePenis);
 console.log("test");
 
-
 //string "thing in quotes"
 //numbers 1232434
 //boolean true or false
-//arrays
-//objects
+//arrays []
+//objects {}
 //functions
+//using `` lets you type words and insert variables like like 27
+
 
 let age = 25;
-console.log(age)
+console.log(age);
 
-console.log(`I can type words and then randomly put a variable here: ${largePenis}`);
+console.log(
+  `I can type words and then randomly put a variable here: ${largePenis}`
+);
 
-console.log("this is largepenis", largePenis)
+console.log("this is where the sample bug is line 32: ", largePenis);
 
+//arrays: keyword is BRACKETS []. list of stuff seperated by commas. not like csv
+
+let myArray = ["word", 3, true, "other words"];
+
+console.log(myArray);
+
+//objects: keyword CURLY BRACES {}. a bunch of unique keys with associated value pairs. meant to give more details about one thing, like a dictionary
+
+//array example: list of people's names
+//onject example: a bunch of properties related to a single person. like name, age, height, etc.
+//arrays can contain objects :: objects can contain arrays
+
+let myObject = {
+  name: largePenis,
+  age: age,
+  array: [1, 2, 3, 4, 5],
+};
+
+let objectArray = [{ name: "whatever" }, 5];
+
+console.log(myObject);
+
+let myLargeCock = ["Drake", "Kainen", "Leo"];
+console.log(myLargeCock);
+
+let kainenObject = {
+  Name: "6'4",
+  Cock: '2"',
+  location: "urMoms",
+  Time: "whenYoureNotHome",
+};
+console.log(kainenObject);
